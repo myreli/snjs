55f23b62adf597a820818597ac890be37e07c2b8
Johnny Almonte <johnny243@users.noreply.github.com>
2022-02-01 17:43:10 -0400

    feat: get icon and tint from NoteType

--- packages/snjs/lib/ui/icons_controller.ts
+++ packages/snjs/lib/ui/icons_controller.ts
@@ -1,5 +1,6 @@
-import { FeatureIdentifier } from '@standardnotes/features';
+import { FeatureIdentifier, NoteType } from '@standardnotes/features';
 import { IconType } from '@Lib/types';
+import { SNComponent } from '@Models/app/component';
 
 export class IconsController {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -11,9 +12,9 @@ export class IconsController {
   }
 
   getIconAndTintForEditor(
-    identifier: FeatureIdentifier | undefined
+    editor?: SNComponent
   ): [IconType, number] {
-    switch (identifier) {
+    switch (editor?.identifier) {
       case FeatureIdentifier.BoldEditor:
       case FeatureIdentifier.PlusEditor:
         return ['rich-text', 1];
@@ -30,6 +31,25 @@ export class IconsController {
         return ['tasks', 3];
       case FeatureIdentifier.CodeEditor:
         return ['code', 4];
+      default:
+        return this.getIconAndTintFromNoteType(editor?.package_info.note_type)
+    }
+  }
+
+  private getIconAndTintFromNoteType(noteType?: NoteType): [IconType, number] {
+    switch (noteType) {
+      case NoteType.Authentication:
+        return ['authenticator', 6];
+      case NoteType.Code:
+        return ['code', 4];
+      case NoteType.Markdown:
+        return ['markdown', 2];
+      case NoteType.RichText:
+        return ['rich-text', 1];
+      case NoteType.Spreadsheet:
+        return ['spreadsheets', 5];
+      case NoteType.Task:
+        return ['tasks', 3];
       default:
         return ['plain-text', 1];
     }
